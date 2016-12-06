1447ab28ddee5627bba34a2f4b053db47cff4f98
in0rdr <andreas.gruhler@uzh.ch>
2016-12-06 11:30:04 +0100

    Revert "Kill global state with pointer-passing"
    This reverts commit 70838abe942dedc20f50cab1efe11491b91906a7.

--- diary.h
+++ diary.h
@@ -16,14 +16,12 @@
 
 static const char* WEEKDAYS[] = {"Mo","Tu","We","Th","Fr","Sa","Su", NULL};
 
-struct app_state;
-
-void setup_cal_timeframe(struct app_state*);
+void setup_cal_timeframe();
 void draw_wdays(WINDOW* head);
-void draw_calendar(struct app_state*, WINDOW* number_pad, WINDOW* month_pad, char* diary_dir, size_t diary_dir_size);
-void update_date(struct app_state*, WINDOW* header);
+void draw_calendar(WINDOW* number_pad, WINDOW* month_pad, char* diary_dir, size_t diary_dir_size);
+void update_date(WINDOW* header);
 
-bool go_to(struct app_state*, WINDOW* calendar, WINDOW* aside, time_t date, int* cur_pad_pos);
+bool go_to(WINDOW* calendar, WINDOW* aside, time_t date, int* cur_pad_pos);
 void display_entry(char* dir, size_t dir_size, struct tm* date, WINDOW* win, int width);
 void edit_cmd(char* dir, size_t dir_size, struct tm* date, char* rcmd, size_t rcmd_size);
 
